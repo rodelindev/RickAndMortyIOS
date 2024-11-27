8a54af1965707a2bb11d6951bb941758c8870c7d
RodelinDev <rdbivieca@gmail.com>
2024-11-27 17:42:08 -0400

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Rodelin Dipre on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
